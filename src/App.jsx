fd879e2588acebe6376db62e8b30735054f2c945
Javi <xabierjc@gmail.com>
2026-05-04 18:03:54 +0200

    fix: use HashRouter instead of BrowserRouter for reliable Vercel routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import { NotesProvider } from './context/NotesContext'
 import LoginPage from './pages/LoginPage'
@@ -55,10 +55,10 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
